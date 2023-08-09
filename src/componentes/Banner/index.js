4b8ca80df1b03df7a213929db58d8ff4fe78b4a6
Larissa Anielly Freitas Moura <lnielly.freitas@gmail.com>
2023-08-08 21:06:54 -0300

    0.0.0

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+const Banner = () => {
+    //JSX -> Como o react lê o js e escreve no DOM
+    return(
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="Banner Principal da pag"></img>
+        </header>  
+    )
+}
+
+export default Banner
